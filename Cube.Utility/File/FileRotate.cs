c4ae1dbcbfeb4086d36bd8c96eb9dcb88a3b9c4e
cris <cristic1980@gmail.com>
2023-04-27 01:33:06 +0900

    Add file utility class

---
+++ Cube.Utility/File/FileRotate.cs
@@ -0,0 +1,170 @@
+﻿using System.Xml.Linq;
+
+namespace Cube.Utility;
+
+public class FileRotate
+{
+    public FileRotate()
+    {
+        //TODO
+    }
+
+    public void FileRotateType1(string logDirectory, string logFile, int maxBackupFiles)
+    {
+        
+        if (string.IsNullOrWhiteSpace(logDirectory))
+            throw new ArgumentNullException(nameof(logDirectory), $"{nameof(logDirectory)} cannot be null");
+        if (string.IsNullOrWhiteSpace(logFile))
+            throw new ArgumentNullException(nameof(logFile), $"{nameof(logFile)} cannot be null");
+        if (maxBackupFiles <= 0)
+            throw new ArgumentNullException(nameof(maxBackupFiles), $"{nameof(maxBackupFiles)} cannot be less than 0");
+
+        // Get the full path of the log file
+        string logFilePath = Path.Combine(logDirectory, logFile);
+        string backupFilePath = string.Empty;
+
+        try
+        {
+            // Check if the log file exists
+            if (File.Exists(logFilePath))
+            {
+                // Rename the current log file
+                for (int i = maxBackupFiles - 2; i >= 0; i--)
+                {
+                    backupFilePath = Path.Combine(logDirectory, $"{logFile}.{i}");
+                    if (File.Exists(backupFilePath))
+                    {
+                        string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{i + 1}");
+                        File.Move(backupFilePath, newBackupFilePath, true);
+                    }
+                }
+
+                string lastBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{maxBackupFiles - 1}");
+                if (File.Exists(lastBackupFilePath))
+                {
+                    File.Delete(lastBackupFilePath);
+                }
+
+                backupFilePath = Path.Combine(logDirectory, $"{logFile}.0");
+                File.Move(logFilePath, backupFilePath, true);
+            }
+
+            // Create or append to the log file
+            using (StreamWriter sw = File.AppendText(logFilePath))
+            {
+                sw.WriteLine($"[{DateTime.Now.ToString()}] This is a log message.");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }        
+    }
+
+    public void FileRotateType2(string logDirectory, string logFile, int maxBackupFiles)
+    {
+        if (string.IsNullOrWhiteSpace(logDirectory))
+            throw new ArgumentNullException(nameof(logDirectory), $"{nameof(logDirectory)} cannot be null");
+        if (string.IsNullOrWhiteSpace(logFile))
+            throw new ArgumentNullException(nameof(logFile), $"{nameof(logFile)} cannot be null");
+        if (maxBackupFiles <= 0)
+            throw new ArgumentNullException(nameof(maxBackupFiles), $"{nameof(maxBackupFiles)} cannot be less than 0");
+
+        // Get the full path of the log file
+        string logFilePath = Path.Combine(logDirectory, logFile);
+        string backupFilePath = string.Empty;
+
+        try
+        {
+            // Check if the log file exists and is larger than the maximum file size
+            if (File.Exists(logFilePath))
+            {
+                // Rename the current log file
+                for (int i = maxBackupFiles - 1; i >= 0; i--)
+                {
+                    backupFilePath = Path.Combine(logDirectory, $"{logFile}.{i}");
+                    if (File.Exists(backupFilePath))
+                    {
+                        if (i == maxBackupFiles - 1)
+                        {
+                            File.Delete(backupFilePath);
+                        }
+                        else
+                        {
+                            string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{i + 1}");
+                            File.Move(backupFilePath, newBackupFilePath, true);
+                        }
+                    }
+                }
+
+                backupFilePath = Path.Combine(logDirectory, $"{logFile}.0");
+                File.Move(logFilePath, backupFilePath, true);
+            }
+
+            // Create or append to the log file
+            using (StreamWriter sw = File.AppendText(logFilePath))
+            {
+                sw.WriteLine($"[{DateTime.Now.ToString()}] This is a log message.");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+
+    public async Task FileRotateAsync(string logDirectory, string logFile, int maxBackupFiles)
+    {
+        if (string.IsNullOrWhiteSpace(logDirectory))
+            throw new ArgumentNullException(nameof(logDirectory), $"{nameof(logDirectory)} cannot be null");
+        if (string.IsNullOrWhiteSpace(logFile))
+            throw new ArgumentNullException(nameof(logFile), $"{nameof(logFile)} cannot be null");
+        if (maxBackupFiles <= 0)
+            throw new ArgumentNullException(nameof(maxBackupFiles), $"{nameof(maxBackupFiles)} cannot be less than 0");
+
+        // Get the full path of the log file
+        string logFilePath = Path.Combine(logDirectory, logFile);
+        string backupFilePath = string.Empty;
+
+        try
+        {
+            if (File.Exists(logFilePath))
+            {
+                // Rename the current log file
+                for (int i = maxBackupFiles - 1; i >= 0; i--)
+                {
+                    backupFilePath = Path.Combine(logDirectory, $"{logFile}.{i}");
+                    if (File.Exists(backupFilePath))
+                    {
+                        if (i == maxBackupFiles - 1)
+                        {
+                            //File.Delete(backupFilePath);
+                            await Task.Run(() => File.Delete(backupFilePath));
+                        }
+                        else
+                        {
+                            string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{i + 1}");
+                            //File.Move(backupFilePath, newBackupFilePath);
+                            await Task.Run(() => File.Move(backupFilePath, newBackupFilePath));
+                        }
+                    }
+                }
+
+                backupFilePath = Path.Combine(logDirectory, $"{logFile}.0");
+                //File.Move(logFilePath, backupFilePath);
+                await Task.Run(() => File.Move(logFilePath, backupFilePath));
+            }
+
+            // Create or append to the log file
+            using (StreamWriter sw = File.AppendText(logFilePath))
+            {
+                //sw.WriteLine($"[{DateTime.Now.ToString()}] This is a log message.");
+                await sw.WriteLineAsync($"[{DateTime.Now.ToString()}] This is a log message.");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
--- Cube.Utility/Program.cs
+++ Cube.Utility/Program.cs
@@ -23,7 +23,8 @@ public class Program
         Stopwatch sw = Stopwatch.StartNew();
         for (int i = 0; i < loopLimit; i++)
         {
-            BackupRotate_1(logDirectory, "test_a.log", maxBackupFiles);
+            new FileRotate()
+                .FileRotateType1(logDirectory, logFile, maxBackupFiles);
         }
         sw.Stop();
         Console.WriteLine($"End : Time (ElapsedMilliseconds) : {sw.ElapsedMilliseconds}");
@@ -36,7 +37,8 @@ public class Program
         sw.Start();
         for (int i = 0; i < loopLimit; i++)
         {
-            BackupRotate_2(logDirectory, "test_b.log", maxBackupFiles);
+            new FileRotate()
+                .FileRotateType2(logDirectory, logFile, maxBackupFiles);
         }
         sw.Stop();
         Console.WriteLine($"End : Time (ElapsedMilliseconds) : {sw.ElapsedMilliseconds}");
@@ -48,156 +50,15 @@ public class Program
         Stopwatch sw = Stopwatch.StartNew();
         sw.Start();
         for (int i = 0; i < loopLimit; i++)
-        {
-            await BackupRotateAsync(logDirectory, logFile, maxBackupFiles);
+        {            
+            await new FileRotate()
+                .FileRotateAsync(logDirectory, logFile, maxBackupFiles);
         }        
         sw.Stop();
         Console.WriteLine($"End : Time (ElapsedMilliseconds) : {sw.ElapsedMilliseconds}");
 
 #endif
 
-    }
-
-    static async Task BackupRotateAsync(string logDirectory, string logFile, int maxBackupFiles)
-    {
-        // Get the full path of the log file
-        string logFilePath = Path.Combine(logDirectory, logFile);
-        string backupFilePath = string.Empty;
-
-        // Check if the log file exists and is larger than the maximum file size
-        if (File.Exists(logFilePath))
-        {
-            // Rename the current log file
-            for (int i = maxBackupFiles - 1; i >= 0; i--)
-            {
-                backupFilePath = Path.Combine(logDirectory, $"{logFile}.{i}");
-                if (File.Exists(backupFilePath))
-                {
-                    if (i == maxBackupFiles - 1)
-                    {
-                        //File.Delete(backupFilePath);
-                        await Task.Run(() => File.Delete(backupFilePath));
-                    }
-                    else
-                    {
-                        string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{i + 1}");
-                        //File.Move(backupFilePath, newBackupFilePath);
-                        await Task.Run(() => File.Move(backupFilePath, newBackupFilePath));
-                    }
-                }
-            }
-
-            backupFilePath = Path.Combine(logDirectory, $"{logFile}.0");
-            //File.Move(logFilePath, backupFilePath);
-            await Task.Run(() => File.Move(logFilePath, backupFilePath));
-        }
-
-        // Create or append to the log file
-        using (StreamWriter sw = File.AppendText(logFilePath))
-        {
-            //sw.WriteLine($"[{DateTime.Now.ToString()}] This is a log message.");
-            await sw.WriteLineAsync($"[{DateTime.Now.ToString()}] This is a log message.");
-        }
-    }
-
-    //private static async Task RenameBackupFilesAsync(string logDirectory, string logFile, int maxBackupFiles)
-    //{
-    //    var backupFilePaths = Enumerable.Range(0, maxBackupFiles - 1)
-    //                                    .Select(i => Path.Combine(logDirectory, $"{logFile}.{i}"))
-    //                                    .OrderByDescending(path => path)
-    //                                    .Skip(1);
-
-    //    foreach (var backupFilePath in backupFilePaths)
-    //    {
-    //        string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{int.Parse(Path.GetExtension(backupFilePath).TrimStart('.')) + 1}");
-    //        await MoveLogFileAsync(backupFilePath, newBackupFilePath);
-    //    }
-
-    //    string lastBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{maxBackupFiles - 1}");
-    //    if (File.Exists(lastBackupFilePath))
-    //    {
-    //        await Task.Run(() => File.Delete(lastBackupFilePath));
-    //    }
-    //}
-
-    //private static async Task MoveLogFileAsync(string sourceFilePath, string destFilePath)
-    //{
-    //    await Task.Run(() => File.Move(sourceFilePath, destFilePath));
-    //}
-
-    static void BackupRotate_1(string logDirectory, string logFile, int maxBackupFiles)
-    {
-        // Get the full path of the log file
-        string logFilePath = Path.Combine(logDirectory, logFile);
-        string backupFilePath = string.Empty;
-
-        // Check if the log file exists
-        if (File.Exists(logFilePath))
-        {
-            // Rename the current log file
-            for (int i = maxBackupFiles - 2; i >= 0; i--)
-            {
-                backupFilePath = Path.Combine(logDirectory, $"{logFile}.{i}");
-                if (File.Exists(backupFilePath))
-                {
-                    string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{i + 1}");
-                    File.Move(backupFilePath, newBackupFilePath, true);
-                }
-            }
-            
-            string lastBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{maxBackupFiles - 1}");
-            if (File.Exists(lastBackupFilePath))
-            {
-                File.Delete(lastBackupFilePath);
-            }
-            
-            backupFilePath = Path.Combine(logDirectory, $"{logFile}.0");
-            File.Move(logFilePath, backupFilePath, true);
-        }
-
-        // Create or append to the log file
-        using (StreamWriter sw = File.AppendText(logFilePath))
-        {
-            sw.WriteLine($"[{DateTime.Now.ToString()}] This is a log message.");
-        }
-    }
-
-    static void BackupRotate_2(string logDirectory, string logFile, int maxBackupFiles)
-    {
-        // Get the full path of the log file
-        string logFilePath = Path.Combine(logDirectory, logFile);
-        string backupFilePath = string.Empty;
-
-        // Check if the log file exists and is larger than the maximum file size
-        if (File.Exists(logFilePath))
-        {
-            // Rename the current log file
-            for (int i = maxBackupFiles - 1; i >= 0; i--)
-            {
-                backupFilePath = Path.Combine(logDirectory, $"{logFile}.{i}");
-                if (File.Exists(backupFilePath))
-                {
-                    if (i == maxBackupFiles - 1)
-                    {
-                        File.Delete(backupFilePath);
-                    }
-                    else
-                    {
-                        string newBackupFilePath = Path.Combine(logDirectory, $"{logFile}.{i + 1}");
-                        File.Move(backupFilePath, newBackupFilePath, true);
-                    }
-                }
-            }
-
-            backupFilePath = Path.Combine(logDirectory, $"{logFile}.0");
-            File.Move(logFilePath, backupFilePath, true);
-        }
-
-        // Create or append to the log file
-        using (StreamWriter sw = File.AppendText(logFilePath))
-        {
-            sw.WriteLine($"[{DateTime.Now.ToString()}] This is a log message.");
-        }
     }
 
 }
